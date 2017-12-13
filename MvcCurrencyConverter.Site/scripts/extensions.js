e704c390467672996f59ab97a3ab5de8adbe6d77
Paul Davis <pdavis86@hotmail.co.uk>
2017-12-13 20:23:29 +0000

    Worked on the UI

---
+++ MvcCurrencyConverter.Site/scripts/extensions.js
@@ -0,0 +1,20 @@
+﻿Date.prototype.toShortDateTime = function () {
+    var mm = this.getMonth() + 1; // getMonth() is zero-based
+    var dd = this.getDate();
+    var mins = this.getMinutes();
+    var secs = this.getSeconds();
+
+    var dateSection = [
+        (dd > 9 ? '' : '0') + dd,
+        (mm > 9 ? '' : '0') + mm,
+        this.getFullYear()
+    ].join('/');
+
+    var timeSection = [
+        this.getHours(),
+        (mins > 9 ? '' : '0') + mins,
+        (secs > 9 ? '' : '0') + secs,
+    ].join(':');
+
+    return dateSection + ' ' + timeSection;
+};
--- MvcCurrencyConverter.Site/scripts/main.js
+++ MvcCurrencyConverter.Site/scripts/main.js
@@ -1,32 +1,48 @@
-﻿var exchangeRates = {};
+﻿var currencyConverter = {};
 
-getGbpToEur = function () {
+currencyConverter.getGbpToEur = function () {
+
+    var elRate = document.querySelectorAll('.currency-converter-rate')[0];
+    var elRateDate = document.querySelectorAll('.currency-converter-rate-date')[0];
+    var elError = document.querySelectorAll('.currency-converter-form .error')[0];
+
+    elError.innerHTML = '';
+
+    currencyConverter.currFrom = 'GBP';
+    currencyConverter.currTo = 'EUR';
+    currencyConverter.lastRequest = new Date();
+    
     var request = new XMLHttpRequest();
-    request.open('POST', '/home/GetGbpToEur', true);
+    request.open('POST', 'home/GetGbpToEur', true);
 
     request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
             var data = JSON.parse(request.responseText);
-
-            //console.log(data);
-
-            exchangeRates.gbpToEur = data;
+            currencyConverter.gbpToEur = data;
+            elRate.innerHTML = currencyConverter.gbpToEur;
+            elRateDate.innerHTML = currencyConverter.lastRequest.toShortDateTime();
 
         } else {
             // We reached our target server, but it returned an error
-            console.log('Something went wrong!');
-
+            console.log('Something went wrong!', request.statusText);
+            elError.innerHTML = 'Sorry, something went wrong trying to get the rate. Please try again later.';
         }
     };
 
     request.onerror = function () {
         // There was a connection error of some sort
-
         console.log('Connection error!');
-
+        elError.innerHTML = 'Sorry, something is wrong with the sonnection to our servers. Please try again later.';
     };
 
     request.send();
+}
+
+currencyConverter.getGbpToEurAgain = function () {
+    var milisecondDiff = new Date() - currencyConverter.lastRequest;
+    if (milisecondDiff > 5000) {
+        currencyConverter.getGbpToEur();
+    }
 }
 
 function ready(fn) {
@@ -39,18 +55,22 @@ function ready(fn) {
 
 ready(function () {
 
-    getGbpToEur();
+    currencyConverter.getGbpToEur();
 
-    var elsCurrencyFrom = document.querySelectorAll('.currency-converter-form .currency-from');
-    var elsCurrencyTo = document.querySelectorAll('.currency-converter-form .currency-to');
+    var elsCurrencyFrom = document.querySelectorAll('.currency-from')[0];
+    var elsCurrencyTo = document.querySelectorAll('.currency-to')[0];
 
-    elsCurrencyFrom[0].addEventListener('input', function (e) {
-        //console.log('change made', e.target.value, exchangeRates.gbpToEur);
-        elsCurrencyTo[0].value = (e.target.value * exchangeRates.gbpToEur).toFixed(2);
+    document.querySelectorAll('.currency-from-label')[0].innerHTML = currencyConverter.currFrom;
+    document.querySelectorAll('.currency-to-label')[0].innerHTML = currencyConverter.currTo;
+
+    elsCurrencyFrom.addEventListener('input', function (e) {
+        elsCurrencyTo.value = (e.target.value * currencyConverter.gbpToEur).toFixed(2);
+        currencyConverter.getGbpToEurAgain();
     });
 
-    elsCurrencyTo[0].addEventListener('input', function (e) {
-        //console.log('change made', e.target.value, exchangeRates.gbpToEur);
-        elsCurrencyFrom[0].value = (e.target.value / exchangeRates.gbpToEur).toFixed(2);
+    elsCurrencyTo.addEventListener('input', function (e) {
+        elsCurrencyFrom.value = (e.target.value / currencyConverter.gbpToEur).toFixed(2);
+        currencyConverter.getGbpToEurAgain();
     });
+
 });
